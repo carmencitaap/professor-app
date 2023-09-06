951cd018cad9d6cf0ac696e3f47ebf65ccc2490e
nicobrito <nibrito@miuandes.cl>
2023-09-06 17:09:11 -0400

    feat: models

--- backend/intellicourse/intellicourse_api/models.py
+++ backend/intellicourse/intellicourse_api/models.py
@@ -3,8 +3,31 @@ from django.db import models
 # Create your models here.
 
 class Question(models.Model):
+    TYPE_CHOICES = (
+        ('MC', 'Multiple Choice'),
+        ('N', 'Numeric'),
+    )
+
     question = models.CharField(max_length=100)
+    type_question = models.CharField(max_length=100, choices=TYPE_CHOICES)
+    difficulty = models.IntegerField(default='Easy')
+
+class Alternative(models.Model):
+    question = models.ForeignKey(Question, max_length=5, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=100)
+    isCorrect = models.BooleanField(default=False)
+
+
+class AlternativeQuestion(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    alternatives = models.ManyToManyField(Alternative)
     
+
+class NumericQuestion(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.IntegerField(default=0)
+    
+
 class Student(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
@@ -18,34 +41,37 @@ class Student(models.Model):
     questions_correct = models.ManyToManyField(Question)
     questions_pased = models.ManyToManyField(Question)
 
+    @property
+    def get_level(self):
+        return self.level
+    
+    @property
+    def get_xp(self):
+        return self.xp
+
 
 class Task(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.TextField()
     description = models.CharField(max_length=100)
+
     xp = models.IntegerField(default=0)
     level = models.IntegerField(default=0)
     difuculty = models.IntegerField(default='Easy')
-    n_questions = models.IntegerField(default='Easy')
     questions = models.ManyToManyField(Question)
 
     wrong_answer = models.IntegerField(default=0)
 
-
-
-class Alternative(models.Model):
-    answer = models.CharField(max_length=100)
-    isCorrect = models.BooleanField(default=False)
-
-class AlternativeQuestion(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    alternatives = models.ManyToManyField(Alternative)
-    difficulty = models.IntegerField(default=0)
-
-class NumericQuestion(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    answer = models.IntegerField(default=0)
-    difficulty = models.IntegerField(default='Easy')
+    @property
+    def n_questions(self):
+        return self.questions.count()
     
+    @property
+    def n_correct(self):
+        return self.questions_correct.count()
+
+
+
+
 
 
 
