17f2560640340fe7d6b0dd3b1615e618fe78561d
carmencitaap <cmavendano@miuandes.cl>
2023-09-24 17:39:06 -0300

    feat: ref to home when clicking 'Intellicourse'

--- frontend/professor-app/src/components/NavBar.tsx
+++ frontend/professor-app/src/components/NavBar.tsx
@@ -5,7 +5,9 @@ function NavBar() {
     <nav className="bg-white border-gray-200 dark:bg-gray-900 mb-5">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <div className="flex items-center">
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">IntelliCourse</span>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              <a href="/">IntelliCourse</a>
+            </span>
         </div>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
