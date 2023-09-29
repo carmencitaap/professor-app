021c45c77c62dc2085136cbaf85c653e95439aea
carmencitaap <cmavendano@miuandes.cl>
2023-09-28 23:23:27 -0300

    fix

--- frontend/professor-app/src/components/GeneralDashboard.tsx
+++ frontend/professor-app/src/components/GeneralDashboard.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import CheckQuestionsByAns from '../api/Questions/CheckQuestionsByAnswer';
 import StudentsAndProgress from '../api/Students/StudentsAndProgress';
-// import ExceptionalQuestion from '../api/Questions/ExceptionalQuestion';
+import ExceptionalQuestion from '../api/Questions/ExceptionalQuestion';
 
 function GeneralDashboard() {
     return (
         <div>
             <StudentsAndProgress/>
             <CheckQuestionsByAns/>
-            {/* <ExceptionalQuestion/> */}
+            <ExceptionalQuestion/>
         </div>
     )
 }
